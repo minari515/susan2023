462d6eb507706496ebedbeb6ab769f2c4f2e1d87
minari515 <126942302+minari515@users.noreply.github.com>
2023-05-17 14:47:29 +0900

    API情報の確認

--- front/src/pages/api/v1/line/webhook/handlers/handleTextgpt.ts
+++ front/src/pages/api/v1/line/webhook/handlers/handleTextgpt.ts
@@ -25,39 +25,38 @@ const handleTextgpt = async (message: TextEventMessage, replyToken: string) => {
    * gptによる回答生成
    */
   const apiKey = process.env.OPENAI_API_KEY;
-  console.log(apiKey)
   if (apiKey) {
     console.log("true");
   } else {
     console.log("false");
   }
-  // const configuration = new Configuration({
-  //   apiKey: apiKey,
-  // });
-  // const openai = new OpenAIApi(configuration);
-  // console.log(openai);
-  // try {
-  //   const response = await openai.createChatCompletion({
-  //     model: "gpt-3.5-turbo",
-  //     messages: [{ role: "assistant", content: message.text }],
-  //   });
-  //   console.log(response);
+  const configuration = new Configuration({
+    apiKey: apiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+  console.log(openai);
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "assistant", content: message.text }],
+    });
+    console.log(response);
 
-  //   if (response.data.choices && response.data.choices.length > 0) {
-  //     if (response.data.choices[0].message) {
-  //       const res = response.data.choices[0].message.content;
-  //       replyMessage = [
-  //         {
-  //           type: "text",
-  //           text: res,
-  //         } as TextMessage,
-  //       ];
-  //       console.log(replyMessage);
-  //     }
-  //   }
-  // } catch (error) {
-  //   console.error("GPTのリクエストエラー:", error);
-  // }
+    if (response.data.choices && response.data.choices.length > 0) {
+      if (response.data.choices[0].message) {
+        const res = response.data.choices[0].message.content;
+        replyMessage = [
+          {
+            type: "text",
+            text: res,
+          } as TextMessage,
+        ];
+        console.log(replyMessage);
+      }
+    }
+  } catch (error) {
+    console.error("GPTのリクエストエラー:", error);
+  }
 
   /**
    * LINE botから返信した結果
