2c145af0b13ab155139cec83897a8304be576cf8
minari515 <126942302+minari515@users.noreply.github.com>
2023-05-24 13:59:55 +0900

    プログラム関係の質問に対する自動回答の実装

--- front/src/pages/api/v1/line/webhook/handlers/handleTextgpt.ts
+++ front/src/pages/api/v1/line/webhook/handlers/handleTextgpt.ts
@@ -36,7 +36,7 @@ const handleTextgpt = async (message: TextEventMessage, replyToken: string) => {
   const openai = new OpenAIApi(configuration);
   console.log(openai);
   try {
-    const response = await openai.createChatCompletion({
+    const response_cat = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
         {
@@ -73,53 +73,146 @@ const handleTextgpt = async (message: TextEventMessage, replyToken: string) => {
         },
       ],
     });
-    console.log(response);
+    console.log(response_cat);
 
-    if (response.data.choices && response.data.choices.length > 0) {
-      if (response.data.choices[0].message) {
-        const res = response.data.choices[0].message.content;
-        if (res.match(/chatbotシステムに関する質問/)) {
+    if (response_cat.data.choices && response_cat.data.choices.length > 0) {
+      if (response_cat.data.choices[0].message) {
+        const res_cat = response_cat.data.choices[0].message.content;
+        if (res_cat.match(/chatbotシステムに関する質問/)) {
           replyMessage = [
             {
               type: "text",
               text: "chatbotシステム",
             } as TextMessage,
           ];
-        } else if (res.match(/授業に関する質問/)) {
+        } else if (res_cat.match(/授業に関する質問/)) {
           replyMessage = [
             {
               type: "text",
               text: "じゅぎょう",
             } as TextMessage,
           ];
-        } else if (res.match(/課題に関する質問/)) {
+        } else if (res_cat.match(/課題に関する質問/)) {
           replyMessage = [
             {
               type: "text",
               text: "課題",
             } as TextMessage,
           ];
-        } else if (res.match(/エラーに関する質問/)) {
-          replyMessage = [
-            {
-              type: "text",
-              text: res,
-            } as TextMessage,
-          ];
-        } else if (res.match(/データの前処理に関する質問/)) {
-          replyMessage = [
-            {
-              type: "text",
-              text: res,
-            } as TextMessage,
-          ];
-        } else if (res.match(/プログラム自体に関する質問/)) {
-          replyMessage = [
-            {
-              type: "text",
-              text: res,
-            } as TextMessage,
-          ];
+        } else if (res_cat.match(/エラーに関する質問/)) {
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content:
+                  "あなたは教師であり，学生の質問に回答する必要があります．\
+                  送られた質問に対して，回答を返信してください\
+                  \
+                  質問の中には回答するにあたって情報が不十分な場合があります．\
+                  その際は情報が足りない旨を伝え，\
+                  回答するために必要な情報を追記するように催促してください．"
+              },
+              { role: "assistant", content: message.text },
+              {
+                role: "user",
+                content: "この質問内容への解答を返してください",
+              },
+            ],
+          });
+          if (response.data.choices && response.data.choices.length > 0) {
+            if (response.data.choices[0].message) {
+              replyMessage = [
+                {
+                  type: "text",
+                  text: response.data.choices[0].message.content,
+                } as TextMessage,
+              ];
+            }
+          }else{
+            replyMessage = [
+              {
+                type: "text",
+                text: res_cat,
+              } as TextMessage,
+            ];
+          }
+        } else if (res_cat.match(/データの前処理に関する質問/)) {
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content:
+                  "あなたは教師であり，学生の質問に回答する必要があります．\
+                  送られた質問に対して，回答を返信してください\
+                  \
+                  質問の中には回答するにあたって情報が不十分な場合があります．\
+                  その際は情報が足りない旨を伝え，\
+                  回答するために必要な情報を追記するように催促してください．"
+              },
+              { role: "assistant", content: message.text },
+              {
+                role: "user",
+                content: "この質問内容への解答を返してください",
+              },
+            ],
+          });
+          if (response.data.choices && response.data.choices.length > 0) {
+            if (response.data.choices[0].message) {
+              replyMessage = [
+                {
+                  type: "text",
+                  text: response.data.choices[0].message.content,
+                } as TextMessage,
+              ];
+            }
+          }else{
+            replyMessage = [
+              {
+                type: "text",
+                text: res_cat,
+              } as TextMessage,
+            ];
+          }
+        } else if (res_cat.match(/プログラム自体に関する質問/)) {
+          const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content:
+                  "あなたは教師であり，学生の質問に回答する必要があります．\
+                  送られた質問に対して，回答を返信してください\
+                  \
+                  質問の中には回答するにあたって情報が不十分な場合があります．\
+                  その際は情報が足りない旨を伝え，\
+                  回答するために必要な情報を追記するように催促してください．"
+              },
+              { role: "assistant", content: message.text },
+              {
+                role: "user",
+                content: "この質問内容への解答を返してください",
+              },
+            ],
+          });
+          if (response.data.choices && response.data.choices.length > 0) {
+            if (response.data.choices[0].message) {
+              replyMessage = [
+                {
+                  type: "text",
+                  text: response.data.choices[0].message.content,
+                } as TextMessage,
+              ];
+            }
+          }else{
+            replyMessage = [
+              {
+                type: "text",
+                text: res_cat,
+              } as TextMessage,
+            ];
+          }
         }
         console.log(replyMessage);
       }
